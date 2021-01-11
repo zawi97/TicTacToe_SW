09445d9e53aa0b8a05f8f6fccb255a703ad4ac37
zawi97 <zawi1997@gmail.com>
2021-01-11 08:55:31 +0100

    pierwszy commit

---
+++ Projekt_KiK/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Projekt_KiK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Cam_b1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ruch_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(307, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Cam_b1
+            // 
+            this.Cam_b1.Location = new System.Drawing.Point(12, 12);
+            this.Cam_b1.Name = "Cam_b1";
+            this.Cam_b1.Size = new System.Drawing.Size(83, 35);
+            this.Cam_b1.TabIndex = 1;
+            this.Cam_b1.Text = "Kamera";
+            this.Cam_b1.UseVisualStyleBackColor = true;
+            this.Cam_b1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 372);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(26, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Teraz ruch:";
+            // 
+            // ruch_button
+            // 
+            this.ruch_button.Location = new System.Drawing.Point(241, 12);
+            this.ruch_button.Name = "ruch_button";
+            this.ruch_button.Size = new System.Drawing.Size(129, 35);
+            this.ruch_button.TabIndex = 4;
+            this.ruch_button.Text = "RUCH";
+            this.ruch_button.UseVisualStyleBackColor = true;
+            this.ruch_button.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 418);
+            this.Controls.Add(this.ruch_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Cam_b1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Cam_b1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ruch_button;
+    }
+}
+
---
+++ Projekt_KiK/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace Projekt_KiK
+{
+    
+    public partial class Form1 : Form
+    {
+        bool gra = true;
+        int[,] plansza = new int[3, 3]; //0 - puste; 1 - kółko; 2 - krzyżyk
+        int tura = 0;
+        bool startRecording = false;
+        Random rnd = new Random();
+
+
+        Image<Bgr, byte> imagePB1, imagePB2;
+        VideoCapture cap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imagePB1 = new Image<Bgr, byte>(new Size(300, 300));
+            imagePB2 = new Image<Bgr, byte>(new Size(300, 300));
+
+            try
+            {
+                cap = new VideoCapture(0);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            cap.ImageGrabbed += ProcessFrame;
+        }
+        void ProcessFrame(object sender, EventArgs e)
+        {
+            Mat m = new Mat();
+            cap.Read(m);
+            if (m != null)
+            {
+                imagePB1 = m.ToImage<Bgr, byte>();
+                pictureBox1.Image = imagePB1.Bitmap;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gra = true;
+            startRecording = !startRecording;
+            if (startRecording)
+            {
+                cap.Start();
+            }
+            else
+            {
+                cap.Stop();
+                imagePB1.SetZero();
+                pictureBox1.Image = imagePB1.Bitmap;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            tura++;
+            if (gra)
+            {
+                if (tura % 2 == 1)
+                {
+                    textBox1.Text = "O";
+                    try
+                    {
+                        var temp = imagePB1.SmoothGaussian(5).Convert<Gray, byte>().
+                            ThresholdBinaryInv(new Gray(100), new Gray(255));
+
+                        VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                        Mat m = new Mat();
+
+                        CvInvoke.FindContours(temp, contours, m, Emgu.CV.CvEnum.RetrType.External,
+                            Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+                        for (int i = 0; i < contours.Size; i++)
+                        {
+                            double perimiter = CvInvoke.ArcLength(contours[i], true);
+                            VectorOfPoint approx = new VectorOfPoint();
+                            CvInvoke.ApproxPolyDP(contours[i], approx, 0.04 * perimiter, true);
+
+                            CvInvoke.DrawContours(imagePB1, contours, i, new MCvScalar(0, 0, 255), 2);
+
+                            var moments = CvInvoke.Moments(contours[i]);
+                            int x = (int)(moments.M10 / moments.M00);
+                            int y = (int)(moments.M01 / moments.M00);
+
+                            //int plansza_x = Math.Round()
+                            //int plansza_y
+
+                            if (contours.Size > 10)
+                            {
+                                plansza[(x - 50) / 100, (y - 50) / 100] = 2;
+                            }
+
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+
+                }
+                else
+                {
+                    bool losowanie = true;
+                    textBox1.Text = "X";
+
+                    while (losowanie)
+                    {
+                        int rand_x = rnd.Next(3);
+                        int rand_y = rnd.Next(3);
+                        if (plansza[rand_x, rand_x] == 0)
+                        {
+                            plansza[rand_x, rand_x] = 2;
+                            losowanie = false;
+                        }
+                    }
+
+                    czy_wygrana();
+                    rysowanie();
+                }
+            }
+        }
+
+        private void czy_wygrana() 
+        { 
+            
+        }
+
+        private void rysowanie()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Point P = new Point();
+                    P.X = x * 100 + 50; P.Y = y * 100 + 50;
+                    switch (plansza[x,y])
+                    {
+                        case 1:
+                            CvInvoke.Circle(imagePB2, P, 50, new MCvScalar(255, 0, 0), 5);
+                            break;
+                        case 2:
+
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            pictureBox2.Image = imagePB2.Bitmap;
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
